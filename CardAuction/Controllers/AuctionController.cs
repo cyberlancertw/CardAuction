408553b609359d2970a395d25d4a043dfc01ace4
cyberlancer <cyberlancer@gmail.com>
2022-03-02 23:53:03 +0800

    Add Auction/Post

--- CardAuction/Controllers/AuctionController.cs
+++ CardAuction/Controllers/AuctionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CardAuction.Models;
 
 namespace CardAuction.Controllers
 {
@@ -10,6 +11,25 @@ namespace CardAuction.Controllers
     {
         // GET: Auction
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Post()
+        {
+            if(Session[CDictionary.SK_User] == null)
+            {
+                return RedirectToAction("Login", "Member");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post(string name)       // use view model
         {
             return View();
         }
