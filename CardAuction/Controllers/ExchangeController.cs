8f9e6317168ac66a94b1915b657ec80cdc89dab6
Sian <ZeroO135691123oxo@gmail.com>
2022-03-14 15:45:35 +0800

    exchange post

--- CardAuction/Controllers/ExchangeController.cs
+++ CardAuction/Controllers/ExchangeController.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CardAuction.Models;
 
 namespace CardAuction.Controllers
 {
     public class ExchangeController : Controller
     {
+        dbCardAuctionEntities db = new dbCardAuctionEntities();
+
         // GET: Exchange
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Post()
         {
             return View();
         }
