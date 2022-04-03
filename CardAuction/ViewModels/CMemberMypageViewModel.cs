0916a2c5e980f5e40f7e3407d06e3808d7d3388e
f0253039 <f0253039@gmail.com>
2022-04-03 16:43:05 +0800

    MyPage pagelist

--- CardAuction/ViewModels/CMemberMypageViewModel.cs
+++ CardAuction/ViewModels/CMemberMypageViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using CardAuction.Models;
+using PagedList.Mvc;
+using PagedList;
 
 namespace CardAuction.ViewModels
 {
@@ -10,13 +12,13 @@ namespace CardAuction.ViewModels
     {
         public tMember MyAccount { get; set; }
 
-        public List<tAuctionItem> MyAuctionFavorite { get; set; }
+        public IPagedList<tAuctionItem> MyAuctionFavorite { get; set; }
 
-        public List<tExchangeItem> MyExchangeFavorite { get; set; }
+        public IPagedList<tExchangeItem> MyExchangeFavorite { get; set; }
 
-        public List<tAuctionItem> myAuctionItem { get; set; }
+        public IPagedList<tAuctionItem> myAuctionItem { get; set; }
 
-        public List<tExchangeItem> myExchangeItem { get; set; }
+        public IPagedList<tExchangeItem> myExchangeItem { get; set; }
         //public List<tItem> myAuctionItem { get;set }
     }
 }
